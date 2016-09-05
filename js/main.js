b73ffcf3e959868e5068b387a5980eb1a3d42231
Oscar Toribio <otoribio@intellisys.com.do>
2016-09-05 17:41:47 -0400

    refactoring javascript code to OOP

--- js/main.js
+++ js/main.js
@@ -1,30 +1,71 @@
 "use strict";
+var application = {
 
-document.onclick = function(event){
-	var theTarget = event.target;
-	if (theTarget.className == "item"){
-		setPieceInTurn();
-		theTarget.innerText = getPieceInTurn();
-	}
-}
+	positions: ["c","b","d","e","f","g","h","i","j"],
 
-var pieces = {
-	first_piece: "X",
-	second_piece: "O",
+	pieces:  {
+		first_piece: "X",
+		second_piece: "O",
+	},
+
+	local_storage_keys: {
+		current_piece: "currentPiece",
+		virtual_board: "virtualBoard",
+	},
+
+	init: function(){
+		window.onload = function(){
+			var arr = new Array();
+			arr.push('hi','ho','hu');
+			if (this.readItem(this.local_storage_keys.virtual_board) === null) {
+				console.log("hola mundo")
+				var theArr = JSON.stringify(arr)
+				this.saveItem(this.local_storage_keys.virtual_board, theArr);
+			}
+			this.clickWatcher();
+		}.bind(application)
+	},
+
+	clickWatcher: function(){
+		document.onclick = function(event){
+			var theTarget = event.target;
+			if (theTarget.className == "item"){
+				this.setPieceInTurn();
+				theTarget.innerText = this.getPieceInTurn();
+				console.log(this.positions[0]);
+				var localStorageArray = JSON.parse(this.readItem(this.local_storage_keys.virtual_board))
+				console.log(localStorageArray[0])
+			}
+		}.bind(application)
+	},
+
+	getPieceInTurn:function(){
+		return this.readItem(this.local_storage_keys.current_piece)
+	},
+
+	setPieceInTurn: function (){
+		var piece_in_turn = this.readItem(this.local_storage_keys.current_piece)
+		
+		if (piece_in_turn === null){
+			this.saveItem(this.local_storage_keys.current_piece, this.pieces.first_piece)
+		}else if (piece_in_turn === this.pieces.first_piece){
+			this.saveItem(this.local_storage_keys.current_piece, this.pieces.second_piece)
+		}else if (piece_in_turn === this.pieces.second_piece){
+			this.saveItem(this.local_storage_keys.current_piece, this.pieces.first_piece)
+		}
+	},
+
+	saveItem: function(key,value){
+		localStorage.setItem(key,value);
+	},
+
+	readItem: function(key){
+		return localStorage.getItem(key);
+	},
 };
 
-function getPieceInTurn(){
-	return localStorage.getItem("currentPiece");
-}
+application.init();
 
-function setPieceInTurn(){
-	var piece_in_turn = localStorage.getItem("currentPiece");
-	
-	if (piece_in_turn === null){
-		localStorage.setItem("currentPiece", pieces.first_piece);
-	}else if (piece_in_turn === pieces.first_piece){
-		localStorage.setItem("currentPiece", pieces.second_piece)
-	}else if (piece_in_turn === pieces.second_piece){
-		localStorage.setItem("currentPiece", pieces.first_piece)
-	}
-}
+// document.getElementById("myBtn").addEventListener("click", function(){
+//     this.style.backgroundColor = "red";
+// });
