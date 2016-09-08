e879eb22f6e9767fb4c8e65b51f1fda604dca97b
Oscar Toribio <otoribio@intellisys.com.do>
2016-09-08 18:06:01 -0400

    solving issues about infinite loop

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var application = {
 	
 	board: {
-		positions: ["c","b","d","e","f","g","h","i","j"],
+		winner_checker: ["c","b","d","e","f","g","h","i","j"],
 		board_status: ["","","","","","","","",""],
 		pieces:{
 			first: "X",
@@ -14,6 +14,7 @@ var application = {
 		current_piece: "currentPiece",
 		virtual_board: "virtualBoard",
 		game_status: "gameStatus",
+		win_reference: "winnerChecker",
 	},
 
 	init: function(){
@@ -35,44 +36,62 @@ var application = {
 		}
 	},
 
-	// winnerWatcher: function(current_game){
-		
-	// 	// Check diagonal winner
-	// 	if ((current_game[0] === current_game[4]) && (current_game[4] === current_game[8])){
+	winnerWatcher: function(current_game){
+		// Check diagonal winner
+		if ((current_game[0] === current_game[4]) && (current_game[4] === current_game[8])){
+			alert("has ganado");
+			this.resetBoard();
+		}else if ((current_game[2] === current_game[4]) && (current_game[4] === current_game[6])){
+			alert("has ganado");
+			this.resetBoard();
+		}
 
-	// 	}else if ((current_game[2] === current_game[4]) && (current_game[4] === current_game[6])){
-			
-	// 	}
-
-	// 	// Check horizontal winner
-	// 	for (var i = 0; i < current_game.length ; i+3){
-	// 		(current_game[i] === current_game[i+1]) && (current_game[i+1] === current_game[i+2])
-	// 	}
-	// 	// Check vertical winner
-	// 	for (var i = 0; i < current_game.length ; i++){
-	// 		(current_game[i] === current_game[i+3]) && (current_game[i+3] === current_game[i+6])
-	// 	}
-
-
-	// },
+		// Check horizontal winner
+		for (var i = 0; i < current_game.length ; i += 3){
+			if ((current_game[i] === current_game[i+1]) && (current_game[i+1] === current_game[i+2])){
+				alert("has ganado");
+				this.resetBoard();
+			}
+		}
+		// Check vertical winner
+		for (var i = 0; i < current_game.length ; i++){
+			if ((current_game[i] === current_game[i+3]) && (current_game[i+3] === current_game[i+6])){
+				alert("has ganado");
+				this.resetBoard();
+			}
+		}
+	},
 
 	playInFreeSpace: function(free_space, index){
 		if (free_space.innerHTML == "") {
+			
 			var piece = this.getPieceInTurn();
-			var current_status_available = this.readItem(this.local_storage_keys.game_status);
-			var status_array = JSON.parse(current_status_available);
-			status_array[index] = piece;
-			current_status_available = JSON.stringify(status_array);
-			this.saveItem(this.local_storage_keys.game_status, current_status_available );
 			free_space.innerHTML = piece;
-			this.winnerWatcher(status_array);
+			console.log(piece);
+			var current_status_available = this.readItem(this.local_storage_keys.game_status);
+			var winner_reference = this.readItem(this.local_storage_keys.win_reference);
+			var status_array = JSON.parse(current_status_available);
+			var status_winner = JSON.parse(winner_reference);
+
+			status_array[index] = piece;
+			status_winner[index] = piece;
+			current_status_available = JSON.stringify(status_array);
+			winner_reference = JSON.stringify(status_winner);
+			this.saveItem(this.local_storage_keys.game_status, current_status_available );
+			this.saveItem(this.local_storage_keys.win_reference, winner_reference);
+			var self = this;
+			setTimeout(function(){
+				self.winnerWatcher(status_winner);
+			}, 100);
+			console.log("after winner watcher");
 		}
 	},
 
 	renderCurrentGameStatus: function(){
 		var current_game = this.readItem(this.local_storage_keys.game_status);
 		if (current_game === null){
-			this.saveItem(this.local_storage_keys.game_status, JSON.stringify(this.board.board_status))
+			this.saveItem(this.local_storage_keys.game_status, JSON.stringify(this.board.board_status));
+			this.saveItem(this.local_storage_keys.win_reference, JSON.stringify(this.board.winner_checker))
 		}else{
 
 			var game_status_array = JSON.parse(current_game);
